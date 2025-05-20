edd66133bd803050e3a7d62962acacc8d8d69586
Jeon, Hyeon O <gusdh2510@gmail.com>
2025-05-20 19:37:57 +0900

    feat : 기본 개발 환경 설정

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerController controller;
+    public PlayerStat stat;
+
+    private void Awake()
+    {
+        PlayerManager.Instance.Player = this;
+        controller = GetComponent<PlayerController>();
+        stat = GetComponent<PlayerStat>();
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : Singleton<PlayerManager>
+{
+   
+    public Player player;
+    public Player Player { get { return player; } set { player = value; } }
+    protected override void Awake() => base.Awake();
+}
