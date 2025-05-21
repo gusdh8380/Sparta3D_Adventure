ec8d1ca2252d90165aa0ff8ba2d754bb3dcf8a57
Jeon, Hyeon O <gusdh2510@gmail.com>
2025-05-21 18:21:30 +0900

    fix : 싱글톤 수정

--- Assets/Scripts/Global/Singleton.cs
+++ Assets/Scripts/Global/Singleton.cs
@@ -6,18 +6,36 @@ using UnityEngine;
 public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T instance;
+    private static bool applicationIsQuitting = false;
+
 
     public static T Instance
     {
         get
         {
+            if (applicationIsQuitting)
+            {
+                Debug.LogWarning($"[{typeof(T)}] Instance 이미 파괴되었거나 종료 중입니다.");
+                return null;
+            }
+
             if (instance == null)
             {
-                instance = new GameObject(typeof(T).Name).AddComponent<T>();
+                // 씬 내 기존 객체 탐색
+                instance = FindObjectOfType<T>();
+                if (instance == null)
+                {
+                    // 없으면 새로 생성
+                    var go = new GameObject(typeof(T).Name);
+                    instance = go.AddComponent<T>();
+                    DontDestroyOnLoad(go);
+                }
             }
             return instance;
         }
     }
+
+    public static T Current => instance;
     protected virtual void Awake()
     {
         if (instance == null)
@@ -29,5 +47,9 @@ public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+    protected virtual void OnApplicationQuit()
+    {
+        applicationIsQuitting = true;
     }
 }
